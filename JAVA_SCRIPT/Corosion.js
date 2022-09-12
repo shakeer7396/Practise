fe43ab5b6602c123b042f24eedc731a5ba580734
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-09-13 00:23:07 +0530

    dd

---
+++ JAVA_SCRIPT/Corosion.js
@@ -0,0 +1,17 @@
+var a=10;
+var b="10"
+var c=a+b;
+console.log(c)
+
+
+var a=100;
+var b=50;
+var c=a-b;
+
+console.log()
+
+
+//"10"+10 -->Number converst to string in + operation output="1010"
+//"3"-3 ----> String converts to number in -operation output=0
+
+//Implicit Corosion means i changing value from one Data type to another Data type
---
+++ JAVA_SCRIPT/Hoisting.js
@@ -0,0 +1,39 @@
+// Hoisting is a default behaviour of js it means you intialize the value in top of the console,then your gave below declaration the console it be printed-->hoisting send the declaration to top of the console same as function outside and block scope also ex-->
+
+
+// x=100
+// console.log(x)
+
+// var x;
+// --------
+
+// x=100;
+// get();
+
+// function get(){
+//     console.log(x);
+// }
+// var x;
+//-------------------------------------
+// get();
+// function get(){
+//     x=100;
+//     console.log("hello",x)
+//     var x;
+// }
+// --------------------------
+
+// Hoisting initialization are not supported only variable declaration are supported like you declare above console and initialize below console it will undefined
+
+// var x;
+// console.log(x)
+// x=100;
+// ----------------------
+// function get(){
+//    var x;
+// console.log(x)
+// x=100; 
+// }
+// get();
+
+
