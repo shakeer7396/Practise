549651091f5fb0796c068248fc6b35df8e360e96
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-09-21 06:44:33 +0530

    sdf

---
+++ JAVA_SCRIPT/prb.js
@@ -0,0 +1,119 @@
+// // 'use strict'
+// // function printDetails(){
+// //      console.log(this.name)
+// // }
+// // var person = {
+// // name: 'Haren',
+// //  company: 'masai',
+// //   print: printDetails
+// // }
+// // person.print()
+// // var name = 'Ajay'
+
+// printName('Shoneil')
+// var printName=function(name){
+//     console.log('hello',name)
+// }
+// (function(){
+//     console.log('Hello!')})
+//     ();
+// console.log(typeof[])
+// function getYear () {
+//     var year = new Date().getFullYear()
+//     return year
+//     }
+//     getYear()
+//     console.log(year) // ?
+
+// function value(b){
+//     const a=100;
+//     a=b
+//     return a
+// }
+// console.log(value(100))
+
+// export default class Class extends Component{
+// constructor(props) {
+// super(props)
+// this.state={
+// fname: 'Masai',
+// lname: 'School'
+// }
+
+// console.log(this.state.fname, this.state. lname)
+// }
+// componentDidUpdate(){
+// console.log('First name is:', this.state.fname)
+// }
+// componentWillUnmount(){
+// console.log('Last name is:', this.state.lname)
+// }
+// componentDidMount(){ console.log('Welcome to Masai')
+// }
+// render() {
+// console.log(`Full name is, ${this.state.fname} ${this.state.lname}`);
+
+// return (
+// <div>
+// WELCOME
+// </div>
+// )
+// }
+// }
+
+// var x = 100
+// function alpha(){ var x = 200
+// function beta(){
+// console.log(x)
+// }
+// return beta
+// }
+// var gamma=alpha()
+// gamma()
+
+// printName('Shoneil')
+// var printName
+
+// function printName(name){
+//     console.log("hello",name)
+// }
+// var printName=function(name){
+//     console.log('hey',name)
+// }
+
+// function alpha(){
+//     var x=100;
+//     this.beta()
+// }
+
+// function beta(){
+//     var x=200
+//     console.log(this.x)
+// }
+// alpha()
+
+// const email ="user@gmail"
+// let = "admin"
+
+// email='admin@gmail'
+// name='Super admin'
+
+// let fname='masai'
+// let lname='school'
+
+// const person ={fname,lname}
+
+// function getName(){
+//     name='albert'
+// }
+// getName()
+// console.log(name)
+
+// const arr= [10, 12, 15, 211];
+//  for (var i = 0; i < arr.length; i++) { 
+//     setTimeout(function(){
+//         console.log('Index:'+i+',element:'+arr[i]);
+//      }, 3000);
+//     }
+
+    
