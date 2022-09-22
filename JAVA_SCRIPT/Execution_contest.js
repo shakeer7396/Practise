a0bac670e8f03d040bfeb10d6469c6d3972c3f9f
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-09-22 06:57:15 +0530

    gij

---
+++ JAVA_SCRIPT/Execution_contest.js
@@ -0,0 +1,5 @@
+//Everithing in js is  happpend inside the excution context -->they store 2 steps(memory)(code)
+//memory store all the data stored i key value pairs || code take all the data it can run one line at a time
+// js is syncronous single threaded language--> it can run only 1 line at a time
+
+
