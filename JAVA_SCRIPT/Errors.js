d90c9b9504bac8ad8593cc1e41d8999fc143c27f
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-09-22 07:13:32 +0530

    sdf

---
+++ JAVA_SCRIPT/Errors.js
@@ -0,0 +1,11 @@
+// Type Error --> it is a conflict of declaration error declaring time came conflict like CONST DECLARE AGAIN that time came
+
+
+// SyntaxError --> it is a Syntax have not match js engine syntical rule like LET RE-DCLARATION it through thi stype of error
+
+// ReferenceError ---> this type of error is occured when any reference in not ther in the scope its a temporal dead zone 
+
+// Some goood Practise ---->
+// Avoid var \use let / try use const -where possible\
+// \declare and initialize to the variable use let to top of them
+// Not througing error in window be good coooder
