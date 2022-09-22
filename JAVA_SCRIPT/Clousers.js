19497a919b9f1f9f4c7ec756592ab756525fd140
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-09-22 08:06:29 +0530

    coool

--- JAVA_SCRIPT/Clousers.js
+++ JAVA_SCRIPT/Clousers.js
@@ -1,11 +1,26 @@
-var count=100;
-function clousers(){
-    var name="hello"
-    return function(){
-        console.log(name)
-        console.log(count)
-    }
+// var count=100;
+// function clousers(){
+//     var name="hello"
+//     return function(){
+//         console.log(name)
+//         console.log(count)
+//     }
 
+// }
+// var res=clousers();
+// res()
+// ------------------------------------------------------------------------
+
+function x(){
+    var a=7;
+    function y(){
+        console.log(a)
+    }
+    return y;
 }
-var res=clousers();
-res()
+var z=x();
+console.log(z)
+
+
+//Uses of Closure
+// Module Design Pattern, Currying, Functions like once(fun that can be run only once), memoize, maintaining state in async world, setTimeout, iterators...
---
+++ JAVA_SCRIPT/SetTime_out.js
@@ -0,0 +1,21 @@
+// function x() {
+//     var i = 1;
+//     setTimeout(function() {
+//     console.log(i);
+//     }, 3000);
+//     console.log("This is Hari");
+//    }
+//    x();
+
+function x(){
+    for(var i=1;i<=5;i++){
+        setTimeout(function(){
+            console.log(i);
+            console.log("dekh")
+        },i*2000)
+    }
+console.log("this is trail")
+    
+}
+
+x()
