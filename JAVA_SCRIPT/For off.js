70834ba12d521ea0755a6559e4e0245bd9ababb7
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-09-24 07:11:12 +0530

    dfg

---
+++ JAVA_SCRIPT/For off.js
@@ -0,0 +1,21 @@
+//The  for of statement creates a loop itrating over itrable objects
+
+var chek=["a","b","c",10];
+
+for(let value of chek){
+    console.log(value + " " +"\n")
+}
+
+// -----------------------------------------------
+// 
+// For in loop 
+// it implements [isko obj print karne spl se banaye hai] obj printing 
+
+var obj={
+    firstname:"shaik",
+    secondname:"shakeer",
+    age:22
+}
+for(let key in obj){
+    console.log(key + ":" + obj[key])
+}
--- JAVA_SCRIPT/Functions.js
+++ JAVA_SCRIPT/Functions.js
@@ -24,10 +24,10 @@
 // -----------------------------------------
 // arrow function =>
 // this function is introduced in Es6 version
-function add(a,b){
-    console.log(a+b)
-}
-add(5,5)//normal
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(5,5)//normal
 
 // var add=(a,b)=>{
 //    return a+b;
@@ -38,4 +38,23 @@ add(5,5)//normal
 //     return (a+b)*5
 // }
 // console.log(mult(5,5))
+
+// --------------------------------------------
+// Function expression 
+// when we create function and assign to a variable this is called function expression
+//function expression ko func before call nai kar sakte (func k vupar call karne aatani) this type of hoisting not accepted in js 
+let express=function chek(){
+    console.log("checked")
+}
+express();
+
+// -----------------------------
+
+// Function declaration 
+// function expression ko func before call kar sakte (func k vupar call kar sakte) this type of hoisting is accepted in js
+
+declare()
+function declare(){
+    console.log("Check kare")
+}
 
