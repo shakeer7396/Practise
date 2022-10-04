ffd06010dc4351c720f47249e2b33c9de60c2f99
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-10-04 12:12:39 +0530

    Update Call,Apply,Bind.js

--- JAVA_SCRIPT/Call,Apply,Bind.js
+++ JAVA_SCRIPT/Call,Apply,Bind.js
@@ -3,15 +3,14 @@
 // this method using another objects values,
 // eg- ek place me hoko dusre place me bulaneka
 
-function hello(){
-    console.log(this.name+" "+this.age);
-}
-var obj={
-    name:"shaik",
-    age:22,
-}
+// function calling(){
+//     console.log("hello"+" "+this.name)
+// }
+// var obj={
+//     name:"shaik",
 
-hello.call(obj)
+// }
+// calling.call(obj)
 // function calling(){
 //     console.log("hello " + this.name)
 // }
@@ -42,16 +41,24 @@ hello.call(obj)
 // Apply()
 //the apply method similar to the call method ,the difference is call method takes arguments seperately,but apply method takes an arguments with arrays.
 
-function check(message){
-    console.log(this.name+" "+this.age+message)
-}
-var person={
-    name:"shaik",
-    age:22
-}
-check.apply(person,[" great"])
+// function calling(message){
+//     console.log("hello"+" "+this.name+this.age+message)
+// }
 
+// var obj2={
+//     name:"shakeer",
+//     age:23
+// }
+// calling.apply(obj2,["Good"])
 
+// function check(message){
+//     console.log(this.name+" "+this.age+message)
+// }
+// var person={
+//     name:"shaik",
+//     age:22
+// }
+// check.apply(person,[" great"])
 // --------------------------------------------------------
 // bind()
 // bind method return a new function where the this keyword is bounded to the ownerObject
@@ -64,3 +71,13 @@ check.apply(person,[" great"])
 // var check=bike.details.bind(own,12345,"Yamaha")
 
 // check();
+
+var details={
+    bike:function(brand,number){
+        console.log(this.name+" "+brand+" "+number)
+    }
+    
+}
+var obj={name:"shakeer"}
+var out=details.bike.bind(obj,"shine",4562)
+out();
