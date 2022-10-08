9d9a354941b06209f5bb1513ed824e490acfe59a
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-10-08 09:22:01 +0530

    df

---
+++ exam/index.js
@@ -0,0 +1,42 @@
+
+import React, { useEffect, useState } from"react";
+
+
+//https://jsonplaceholder.typicode.com/todos/1
+const Hello =()=>{
+    const [state,setState]=useState(0)
+    const getData=async()=>{
+        let res=axios.get("https://jsonplaceholder.typicode.com/todos/1")
+        let data=await(res.json)
+    }
+    
+
+    
+
+
+    return(
+
+        <>
+    <div>
+        <button onClick={()=>setState(state+1)}>Inc</button>
+        <button onClick={()=>setState(state-1)}>Red</button>
+    </div>
+
+        </>
+    )
+}
+export default Hello;
+
+
+
+
+let p1=new Person("shaik",23)
+console.log(p1)
+
+function Person(first,age){
+    this.firstName=first;
+    this.age=age;
+}
+const myDetails=newPerson("shaik",23);
+const myFriend=newPerson("raj",23);
+
