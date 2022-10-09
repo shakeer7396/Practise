afb2187561bbc21f26d03d9fc1deb6f7ee524228
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-10-09 21:45:20 +0530

    fg

---
+++ React/countriesFetch with axios/countries/src/Components/Countries.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import styles from "./Countries.module.css";
+const Countries = () => {
+   const [data,setData]=useState([])
+
+//    const getData=()=>{
+//     axios.get("https://restcountries.com/v2/all")
+//     .then((r)=>{
+//         setData(r.data)
+//     })
+//    }
+
+//    useEffect(()=>{
+//     getData()
+//    },[])
+    const getData=async()=>{
+        let res=await fetch("https://restcountries.com/v2/all")
+        let datas=await res.json()
+        setData(datas)
+        console.log(datas)
+    }
+    useEffect(()=>{
+        getData()
+    },[])
+   
+   
+  return (
+    <div>
+        <h1> Countries</h1>
+        <div className={styles.container}>
+            {data?.map((el)=>{
+                return(
+                    <div key={el.id}>
+                    <div>
+                        <img className={styles.image} src={el.flags.png}/>
+                        </div>
+                    <p>Name: {el.name}</p>
+                    <p>Population: {el.population}</p>
+                    <p>Region: {el.region}</p>
+                    </div>
+                )
+            })}
+        </div>
+       </div>
+  )
+}
+
+export default Countries
