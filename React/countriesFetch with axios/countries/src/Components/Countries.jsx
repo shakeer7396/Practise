c9c3d470d8655021a863e9be28a0f8505ac80382
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-10-11 20:25:50 +0530

    sd

--- React/countriesFetch with axios/countries/src/Components/Countries.jsx
+++ React/countriesFetch with axios/countries/src/Components/Countries.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import styles from "./Countries.module.css";
 const Countries = () => {
    const [data,setData]=useState([])
+   const [filter,setFilter]=useState('')
+   const [sort,SetSort]=useState('')
 
    const getData =async()=>{
     axios.get("https://restcountries.com/v2/all")
@@ -28,11 +30,43 @@ const Countries = () => {
     // useEffect(()=>{
     //     getData()
     // },[])
+
+    const handleFilter =(e)=>{
+        setFilter(e.target.value)
+        axios.get(`https://restcountries.com/v2/region/${e.target.value}`)
+        .then((r)=>{
+            setData(r.data)
+        })
+    }
+   
+
+
+    const handleSort =()=>{
+
+    }
    
    
   return (
     <div>
         <h1> Countries</h1>
+        <div className={styles.top}>
+            <div className={styles.filter}>
+            <select name="" id="" value={filter} onChange={handleFilter}>
+                <option value="">Filter By Country</option>
+                <option value="asia">Asia</option>
+                <option value="africa">Africa</option>
+                <option value="europe">Europe</option>
+
+            </select>
+            </div>
+            <div className={styles.sort}>
+            <select name="" id="" value={sort} onChange={handleSort}>
+                <option value="">Sort By Population</option>
+                <option value="lth">Low to High</option>
+                <option value="htl">High to Low</option>
+            </select>
+            </div>
+        </div>
         <div className={styles.container}>
             {data?.map((el)=>{
                 return(
