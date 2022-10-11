1f995d6c3e0c10efbdac53588fb5dd315d0fd7d0
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-10-11 19:09:18 +0530

    gh

--- JAVA_SCRIPT/Prototype.js
+++ JAVA_SCRIPT/Prototype.js
@@ -1,11 +1,12 @@
-function Person(first, last, age, eyecolor) {
+function Person(first, last, age) {
     this.firstName = first;
     this.lastName = last;
     this.age = age;
 
-  Person.prototype.nationality = "English";
 
   }
+  Person.prototype.nationality = "English";
+
   
 let r1=new Person("shaik","shakeer",23)
 console.log(r1)
