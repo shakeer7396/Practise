0a71160234374981d1a2b0b5210746ef5813e6cc
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-10-16 15:35:57 +0530

    add

---
+++ Counter All/Js Counter/index.js
@@ -0,0 +1,18 @@
+
+let counter=document.querySelector(".count");
+let count=0;
+
+function changeCount(name){
+    if(name==inc){
+        count+=1;
+    }
+    else if(name==dec){
+        if(count>0){
+            count-=1
+        }
+    }
+    else if(name==reset){
+        count=0;
+    }
+    counter.innerText = count
+}
