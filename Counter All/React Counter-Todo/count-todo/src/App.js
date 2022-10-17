2fdee8b33514f047dc52fa65b96dc6d970701447
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-10-17 20:45:05 +0530

    gh

---
+++ Counter All/React Counter-Todo/count-todo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter/Counter';
+import Todo from './Todo/Todo';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Counter /> */}
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
