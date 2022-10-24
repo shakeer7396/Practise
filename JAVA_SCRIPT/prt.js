1b88f4ac429915845eba9bed6ca72b35064190e0
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-10-24 21:22:28 +0530

    hj

--- JAVA_SCRIPT/prt.js
+++ JAVA_SCRIPT/prt.js
@@ -19,26 +19,26 @@
 console.log(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13,14,7]));
 
 function Arrays_sum(arr1, arr2){
-  var result = "";
+  var bag = "";
   var sum = 0;
   var i=0;
 
  while (sum < arr1.length && sum < arr2.length) {
-    result+=arr1[sum] + arr2[sum]+" ";
+    bag+=arr1[sum] + arr2[sum]+" ";
     sum++;
   }
 
  if (sum === arr1.length) {
     for (var i = sum; i < arr2.length; i++) {
-      result+=arr2[i]+" ";
+      bag+=arr2[i]+" ";
     }
   } 
   else{
   for (var i = sum; i< arr1.length; i++) {
-      result+=arr1[i]+" ";
+      bag+=arr1[i]+" ";
     }
   }
-  return result;
+  return bag;
 }
 
 
