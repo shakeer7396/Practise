db98b527445728a46227abdc6c29c9f10954ef90
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-10-24 21:39:27 +0530

    df

--- JAVA_SCRIPT/prt.js
+++ JAVA_SCRIPT/prt.js
@@ -16,29 +16,28 @@
 
 // sum of both given array index values and print a new array
 
-console.log(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13,14,7]));
+// console.log(Arrays_sum([1,0,2,3,4], [3,5,6,7,8,13,14,7]));
 
-function Arrays_sum(arr1, arr2){
-  var bag = "";
-  var sum = 0;
-  var i=0;
+// function Arrays_sum(arr1, arr2){
+//   var bag = "";
+//   var sum = 0;
+//   var i=0;
 
- while (sum < arr1.length && sum < arr2.length) {
-    bag+=arr1[sum] + arr2[sum]+" ";
-    sum++;
-  }
+//  while (sum < arr1.length && sum < arr2.length) {
+//     bag+=arr1[sum] + arr2[sum]+" ";
+//     sum++;
+//   }
 
- if (sum === arr1.length) {
-    for (var i = sum; i < arr2.length; i++) {
-      bag+=arr2[i]+" ";
-    }
-  } 
-  else{
-  for (var i = sum; i< arr1.length; i++) {
-      bag+=arr1[i]+" ";
-    }
-  }
-  return bag;
-}
-
+//  if (sum === arr1.length) {
+//     for (var i = sum; i < arr2.length; i++) {
+//       bag+=arr2[i]+" ";
+//     }
+//   } 
+//   else{
+//   for (var i = sum; i< arr1.length; i++) {
+//       bag+=arr1[i]+" ";
+//     }
+//   }
+//   return bag;
+// }
 
