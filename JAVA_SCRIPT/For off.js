9f1af7d673e5bc9b61cbf725136a5e2e5a007938
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-10-24 11:10:03 +0530

    dvfdg

--- JAVA_SCRIPT/For off.js
+++ JAVA_SCRIPT/For off.js
@@ -11,6 +11,15 @@ for(value of chek){
 // For in loop 
 // it implements [isko obj print karne spl se banaye hai] obj printing 
 
+// var obj={
+//     name:"shaik",
+//     age:22
+// }
+
+// for(var key in obj){
+//     console.log(key+" "+obj[key])
+// }
+
 var obj={
     name:"shaik",
     age:22
--- JAVA_SCRIPT/prt.js
+++ JAVA_SCRIPT/prt.js
@@ -1,5 +1,20 @@
 
 
+// var a=100;
+// function out(){
+//     var b=50;
+//     function insd(){
+//         var c=30
+//         console.log(a);
+//         console.log(b);
+//         console.log(c);
+//     }
+//     return insd
+// }
+// var res=out();
+// res();
+
+
 var a=100;
 function out(){
     var b=50;
