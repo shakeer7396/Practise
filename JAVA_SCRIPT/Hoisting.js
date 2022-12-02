926d05b9dff9c525d12b5a7b69df3f114007aebd
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-12-02 07:43:40 +0530

    Update Hoisting.js

--- JAVA_SCRIPT/Hoisting.js
+++ JAVA_SCRIPT/Hoisting.js
@@ -1,16 +1,16 @@
 // Hoisting is a default behaviour of js it means you intialize the value in top of the console,then your gave below declaration the console it be printed-->hoisting send the declaration to top of the console same as function outside and block scope also ex-->
 
 
-// x=100;
-// console.log(x)
-// var x;
+x=100;
+console.log(x)
+var x;
 
-// function hello(){
-//     y=100
-//     console.log(y)
-// }
-// var y;
-// hello()
+function hello(){
+    y=100
+    console.log(y)
+}
+var y;
+hello()
 // x=100;
 // console.log(x);
 //  var x;
@@ -24,16 +24,16 @@
  
 
 //-------------------------------------
-get();
-function get(){
-    x=100;
-    console.log("hello",x)
-    var x;
-}
+// get();
+// function get(){
+//     x=100;
+//     console.log("hello",x)
+//     var x;
+// }
 
-a=100;
-console.log(a)
-var a;
+// a=100;
+// console.log(a)
+// var a;
 // --------------------------
 
 // Hoisting initialization are not supported only variable declaration are supported like you declare above console and initialize below console it will undefined
