230933703a7c5dfd969f74457be08fbb71819f32
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-12-12 19:22:31 +0530

    nm

--- Counter All/Js Counter/index.js
+++ Counter All/Js Counter/index.js
@@ -1,19 +1,26 @@
 
+// let counter=document.querySelector(".count");
+// let count=0;
+
+// function changeCount(name){
+//     if(name==inc){
+//         count+=1;
+//     }
+//     else if(name==dec){
+//         if(count>0){
+//             count-=1
+//         }
+//     }
+//     else if(name==reset){
+//         count=0;
+//     }
+//     counter.innerText = count
+// }
+
 let counter=document.querySelector(".count");
 let count=0;
 
 function changeCount(name){
-    if(name==inc){
-        count+=1;
-    }
-    else if(name==dec){
-        if(count>0){
-            count-=1
-        }
-    }
-    else if(name==reset){
-        count=0;
-    }
-    counter.innerText = count
+    
 }
 
