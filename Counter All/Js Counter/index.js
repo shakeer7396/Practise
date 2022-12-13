8fdedcd43078072ba71be1a8f708b95a1dee8c26
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-12-13 17:42:05 +0530

    Update index.js

--- Counter All/Js Counter/index.js
+++ Counter All/Js Counter/index.js
@@ -1,26 +1,22 @@
 
-// let counter=document.querySelector(".count");
-// let count=0;
 
-// function changeCount(name){
-//     if(name==inc){
-//         count+=1;
-//     }
-//     else if(name==dec){
-//         if(count>0){
-//             count-=1
-//         }
-//     }
-//     else if(name==reset){
-//         count=0;
-//     }
-//     counter.innerText = count
-// }
-
-let counter=document.querySelector(".count");
+let counter=document.querySelector(".count")
 let count=0;
 
-function changeCount(name){
-    
+function changeCount(val){
+    if(val==inc){
+        count+=1
+    }
+    else if(val==dec){
+        if(count>0){
+            count-=1
+        }
+    }
+    else if(val==reset){
+        count=0
+    }
+    counter.innerText=count;
 }
+
+
 
