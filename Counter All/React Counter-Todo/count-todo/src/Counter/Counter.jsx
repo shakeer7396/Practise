65c58e5612e37d5332392b438d74bacfcb37d7e6
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-12-15 15:30:25 +0530

    hj

--- Counter All/React Counter-Todo/count-todo/src/Counter/Counter.jsx
+++ Counter All/React Counter-Todo/count-todo/src/Counter/Counter.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { useState } from 'react'
+// import React from 'react'
+// import { useState } from 'react'
 
-const Counter = () => {
-    const[count,setCount]=useState(0)
-  return (
-    <div>
-        <h1>Counter : {count}</h1>
-        <button onClick={()=>setCount(count+1)}>Increase</button>
-        <button onClick={()=>setCount(count>5?count-1:0)}>Decrease</button>
-    </div>
-  )
-}
+// const Counter = () => {
+//     const[count,setCount]=useState(0)
+//   return (
+//     <div>
+//         <h1>Counter : {count}</h1>
+//         <button onClick={()=>setCount(count+1)}>Increase</button>
+//         <button onClick={()=>setCount(count>5?count-1:0)}>Decrease</button>
+//     </div>
+//   )
+// }
 
-export default Counter
+// export default Counter
 
 
