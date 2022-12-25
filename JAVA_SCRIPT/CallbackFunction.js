31d5352cf0099de0eb99dee0b25f1c362c171edc
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-12-25 17:28:31 +0530

    Update CallbackFunction.js

--- JAVA_SCRIPT/CallbackFunction.js
+++ JAVA_SCRIPT/CallbackFunction.js
@@ -18,12 +18,3 @@ console.log("mult",sum*10)
 
 out(10,5,mult)
 
-// function abc(a,b,operate){
-//     var sum= a+b
-//     operate(sum)
-// }
-// function add(sum){
-//     console.log("addition",sum*5)
-// }
-
-// add(10,5,add)
