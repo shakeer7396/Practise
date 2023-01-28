726914b7108c98612129af36825ec87f36d719e1
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-01-28 11:56:12 +0530

    Update App.js

--- Counter All/React Counter-Todo/count-todo/src/App.js
+++ Counter All/React Counter-Todo/count-todo/src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
-      {/* <Todo /> */}
-      <Data />
+      <Todo />
+      {/* <Data /> */}
     </div>
   );
 }
