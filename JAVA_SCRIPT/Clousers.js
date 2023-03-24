d91bf8c589864b4299382f84783850d1ae8f5d62
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-03-24 12:40:25 +0530

    Update Clousers.js

--- JAVA_SCRIPT/Clousers.js
+++ JAVA_SCRIPT/Clousers.js
@@ -1,69 +1,18 @@
+// How would you write an object for a database containing the following person’s details: Jack, age 45, with hazel eyes and ID number 5000.
 
-//CLOSURE-->CLOSURE IS  A COMBINATION OF FUNCTIONS BUNDLE TOGETHER WITH REFERENCE SURROUNDING  STATE 
-//A CLOUSER GIVES YOU ACCESS TO AN OUTER FUNCTION SCOPE FOR AN INNER FUNCTION
-// var count=100;
-// function clousers(){
-//     var name="hello"
-//     return function(){
-//         console.log(name)
-//         console.log(count)
-//     }
 
+// details={
+//     "name":"Jack",
+//     "age":45,
+//     "hazel eyes and id":5000
 // }
-// var res=clousers();
-// res()
+// console.log(details)
 
-// var a=100;
-// function out(){
-//     var b=50;
-//      function inside(){
-//         var c=20;
-//         console.log(a)
-//         console.log(b)
-//         console.log(c)
-//     }
-//     return inside
+// Write a loop that prints every number divisible by three from 1-200.
 
-// }
-// var res=out();
-// res()
+for(var i=1;i<=200;i++){
 
-// let a=100;
-// function out(){
-//     let b=200
-//    return function (){
-//         let c=50;
-//         console.log(a);
-//         console.log(b)
-//         console.log(c)
-//     }
-// }
-// var res=out();
-// res()
-
-
-
-// let a=100;
-// function out(){
-//     let b=200
-//    return function (){
-//         let c=50;
-//         console.log(a);
-//         console.log(b)
-//         console.log(c)
-//     }
-// }
-// var res=out();
-// res()
-
-// ------------------------------------------------------------------------
-var Person = function(pname){
-    var name=pname
-    this.getName=function(){
-        return name;
+    if(i%3==0){
+        console.log(i)
     }
 }
-var person=new Person("Neelesh")
-console.log(person.getName())
-//Uses of Closure
-// Module Design Pattern, Currying, Functions like once(fun that can be run only once), memoize, maintaining state in async world, setTimeout, iterators...
