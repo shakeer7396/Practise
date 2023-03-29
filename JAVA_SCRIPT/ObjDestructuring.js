0550b7046d63b9b4c5360f6b754060d370f7aae8
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-03-29 17:42:32 +0530

    Update ObjDestructuring.js

--- JAVA_SCRIPT/ObjDestructuring.js
+++ JAVA_SCRIPT/ObjDestructuring.js
@@ -1,14 +1,14 @@
 //Destructuring means to break down a complex structure into simpler parts. With the syntax of destructuring, you can extract smaller fragments from objects and arrays. It can be used for assignments and declaration of a variable.
 
-// var obj={
-//     name:"shaik",
-//     age:22,
-//     gender:"male",
-//     city:"ATP"
-// }
+var obj={
+    name:"shaik",
+    age:22,
+    gender:"male",
+    city:"ATP"
+}
 
-// let {name,age,gender,city}=obj
-// console.log(name+" "+age);
+let {name,age,gender,city}=obj
+console.log(name+" "+age);
 
 
 // var arr=["shaik","shakeer",22]
