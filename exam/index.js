9109dd52e363dbdd3ec28cd024750ff9758e8cf1
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-04-11 16:58:46 +0530

    Update index.js

--- exam/index.js
+++ exam/index.js
@@ -30,17 +30,41 @@
 
 
 
-let p1=new Person("shaik",23)
-console.log(p1)
+// let p1=new Person("shaik",23)
+// console.log(p1)
 
-function Person(first,age){
-    this.firstName=first;
-    this.age=age;
-}
-const myDetails=newPerson("shaik",23);
-const myFriend=newPerson("raj",23);
+// function Person(first,age){
+//     this.firstName=first;
+//     this.age=age;
+// }
+// const myDetails=newPerson("shaik",23);
+// const myFriend=newPerson("raj",23);
 
 // let question = true;
 //  if (question === false)
 //   console.log(0) 
 //   else console.log(10);
+
+const app=require("express") ()
+
+const Middleware1=(req, res, next) => { console.log('Middleware 1'); next();
+
+}
+
+const Middleware2= (req, res, next) => { console.log('Middleware 2'); next()
+}
+const findthereq=(req, res, next) => { console.log('Middleware 3');
+ next()
+}
+
+app.use(Middleware1);
+
+app.get("/", Middleware2, (req, res)=>{
+
+console.log("I am get req") 
+res.send("hello")
+ })
+
+app.listen(8000)
+
+what is the output
