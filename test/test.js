35aabdc78f6cbaee8c4580f7b2c1c03031de1c05
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-04-17 22:23:34 +0530

    exmp

---
+++ test/test.js
@@ -0,0 +1,28 @@
+// const nums = [4,2,5,1];
+// // nums.sort;
+// for(var i=0;i<nums.length;i++){
+//     for(var j=i+1;j<nums.length;j++){
+//         if(nums[i]>nums[j]){
+//            var temp= nums[i];
+//            nums[i]=nums[j];
+//            nums[j]=temp;
+//         }
+//     }
+// }
+
+// nums.forEach(function(el,acc){
+//     console.log(el+4)
+// })
+
+var arr=[1,2,3,4];
+var sum=0;
+    var count=0;
+    
+    for(var i=0;i<arr.length;i++)
+    {
+        sum=sum+arr[i];
+        count++;
+    }
+    average=sum/count
+    
+    console.log(average);
