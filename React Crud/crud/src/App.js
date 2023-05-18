afcf219f22428e1cdaf7e90eb53978b91a907be4
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-05-18 17:39:59 +0530

    process

--- React Crud/crud/src/App.js
+++ React Crud/crud/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
 
 export default class App extends Component {
-  c="John";
   render() {
     return (
       <div>
-        Hello my first class based component (this.c)
+        <Navbar />
+        <News />
       </div>
     )
   }
---
+++ React Crud/crud/src/Components/NewsItem.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export class NewsItem extends Component {
+    render(){
+        return(
+            <div>
+                This is a News Item
+            </div>
+        )
+    }
+}
+
+export default NewsItem;
+
