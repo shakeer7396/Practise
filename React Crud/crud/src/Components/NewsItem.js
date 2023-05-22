534a2247fadf6f35f580e34831dc5555556bb9a0
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-05-22 12:47:42 +0530

    working card

--- React Crud/crud/src/Components/NewsItem.js
+++ React Crud/crud/src/Components/NewsItem.js
@@ -2,9 +2,18 @@ import React, {Component} from 'react';
 
 export class NewsItem extends Component {
     render(){
+      let {title,discription,imageUrl}=this.props;
         return(
             <div>
-                This is a News Item
+               <div className="card" style={{width: '18rem'}}>
+  <img src={imageUrl} className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{discription}</p>
+    <a href="/newsdetails" className="btn btn-primary">Read more</a>
+  </div>
+</div>
+
             </div>
         )
     }
