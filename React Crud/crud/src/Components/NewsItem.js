67fd2f5f7015a7dc7fd11860a136c5bdb79fce7f
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-05-22 14:49:16 +0530

    working

--- React Crud/crud/src/Components/NewsItem.js
+++ React Crud/crud/src/Components/NewsItem.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
 
 export class NewsItem extends Component {
+   
     render(){
-      let {title,discription,imageUrl}=this.props;
+      let {title,discription,imageUrl,newsUrl}=this.props;
         return(
-            <div>
+            <div className='my-3'>
                <div className="card" style={{width: '18rem'}}>
   <img src={imageUrl} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{discription}</p>
-    <a href="/newsdetails" className="btn btn-primary">Read more</a>
+    <a href="/newsdetails" className="btn btn-sm btn-primary">Read more</a>
   </div>
 </div>
 
