b60c5b2ecbd879bac1b7ebc5d9abfae9ac500935
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-05-25 15:43:28 +0530

    Create Class.js

---
+++ JAVA_SCRIPT/Class.js
@@ -0,0 +1,19 @@
+//A class is a type of function, but instead of using the keyword function to initiate it, we use the keyword class, and the properties are assigned inside a constructor() method.
+
+// class Car{
+//     constructor(name){
+//         this.brand=name
+//     }
+// }
+
+// const myCar=new Car("Ford")
+// console.log(myCar.brand)
+
+
+//METHODS IN CLASS
+
+class Car {
+    constructor(name){
+        this.brand=name
+    }
+}
