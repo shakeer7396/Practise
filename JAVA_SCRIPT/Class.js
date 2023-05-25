97b50521f1fea6f196dc48bf370f461c5f48ba93
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-05-25 16:07:26 +0530

    Update Class.js

--- JAVA_SCRIPT/Class.js
+++ JAVA_SCRIPT/Class.js
@@ -10,16 +10,44 @@
 // console.log(myCar.brand)
 
 
+
 //METHODS IN CLASS
+
+// class Car {
+//     constructor(name){
+//         this.brand=name
+//     }
+//     present(){
+//         return "I have a " +this.brand
+//     }
+// }
+// const mycar =new Car("Ford");
+
+// console.log(mycar.present())
+
+
+
+// Class Inheritance
+// To create a class inheritance, use the extends keyword.
+
+// A class created with a class inheritance inherits all the methods from another class
 
 class Car {
     constructor(name){
-        this.brand=name
+        this.brand=name;
     }
     present(){
-        return "I have a " +this.brand
+        return "I have a "+this.brand;
     }
 }
-const mycar =new Car("Ford");
-
-console.log(mycar.present())
+class Model extends Car{
+    constructor(name,mod){
+        super(name);
+        this.model=mod;
+    }
+    show(){
+       return this.present()+ ", it is a "+ this.model
+    }
+}
+const mycar =new Model("Ford","Shift")
+console.log(mycar.show())
