84641240d411809c35df045785d553abcdf421b6
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-06-07 22:11:42 +0530

    practise

--- React/assignment/frnt/src/App.js
+++ React/assignment/frnt/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Axios from './Component/Axios';
+import Fetch from './Component/Fetch';
 
 function App() {
   return (
     <div className="App">
      <h1>Assignment</h1>
+     {/* <Fetch /> */}
+     <Axios />
     </div>
   );
 }
---
+++ React/assignment/frnt/src/Component/Fetch.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+const Fetch = () => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products?limit=50')
+            .then(response => response.json())
+            .then(data => {
+                setData(data)
+            })
+            .catch(error => {
+                setError(error)
+            })
+    }, [])
+
+    if (error) {
+        return <div>Error : {error.message}</div>
+    }
+    else if (!data) {
+        return <div>Loading....</div>
+    }
+    else {
+        return (
+            <div>
+                {data?.map((el) => (
+                    <div key={el.id} style={{ border: "2px solid red", padding: "20px" }}>
+                        <div>
+                            <img src={el.image} alt="check" width="200px" />
+                        </div>
+                        <h2>Title : {el.title}</h2>
+                        <h2>Price : {el.price}</h2>
+                        <h3>Descripton : {el.description}</h3>
+                    </div>
+                ))}
+
+            </div>
+        )
+    }
+
+
+}
+export default Fetch;
