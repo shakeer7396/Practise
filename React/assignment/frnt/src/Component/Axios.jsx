1fd2ba783e8190536136a810268f1fc9b5dc76bb
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-06-07 22:12:38 +0530

    format

--- React/assignment/frnt/src/Component/Axios.jsx
+++ React/assignment/frnt/src/Component/Axios.jsx
@@ -1,31 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Axios = () => {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        axios.get("https://fakestoreapi.com/products?limit=50")
-            .then(response => {
-                setData(response.data)
+        axios
+            .get("https://fakestoreapi.com/products?limit=50")
+            .then((response) => {
+                setData(response.data);
             })
-            .catch(error => {
-                setError(error)
-            })
-    }, [])
+            .catch((error) => {
+                setError(error);
+            });
+    }, []);
 
     if (error) {
-        return <div>Error : {error.message}</div>
-    }
-    else if (!data) {
-        return <div>Loading...</div>
-    }
-    else {
+        return <div>Error : {error.message}</div>;
+    } else if (!data) {
+        return <div>Loading...</div>;
+    } else {
         return (
             <div>
                 {data.map((el) => (
-                    <div key={el.id} style={{ border: "2px solid blue", padding: "20px" }}>
+                    <div
+                        key={el.id}
+                        style={{ border: "2px solid blue", padding: "20px" }}
+                    >
                         <div>
                             <img src={el.image} alt="check" width="200px" />
                         </div>
@@ -35,10 +37,8 @@ const Axios = () => {
                     </div>
                 ))}
             </div>
-        )
+        );
     }
+};
 
-
-}
-
-export default Axios
+export default Axios;
