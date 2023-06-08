aba34a1391f6851f7a758c270a7d462cd6a87232
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-06-08 23:15:27 +0530

    Update AsyncAwait.jsx

--- React/assignment/frnt/src/Component/AsyncAwait.jsx
+++ React/assignment/frnt/src/Component/AsyncAwait.jsx
@@ -1,9 +1,45 @@
 import React, { useState, useEffect } from 'react';
 
 const AsyncAwait = () => {
+
     return (
         <div>AsyncAwait</div>
     )
 }
 
 export default AsyncAwait
+
+
+// const [todo,setTodo]=useState([])
+// //get method...........
+// const getData=async ()=>{
+//     try {
+//         let res = await fetch(`http://localhost:6060/posts?_page=${page}`)
+//         let data= await res.json()
+        
+//         console.log("Output",data)
+//         setTodo(data)
+//     } catch (error) {
+//         console.log(error)
+//     } 
+// }
+// useEffect (()=>{
+//     getData()    
+// },[page])
+
+// return (
+//     <div className={styles.btn}>
+//         <TodoAdd mount={mount}/>
+//         {todo.map((todo)=>(
+//         <Todo key={todo.id} todo={todo} onDelete={onDelete} onEdit={onEdit}/>))}
+//         <p>Page :{page}</p>
+        
+//         <button onClick={()=>{
+//             if(page>1){
+//                 setPage(page-1)
+//             }  
+//         }}>Prev</button>
+
+//         <button onClick={()=>setPage(page+1)}>Next</button>
+//     </div>
+// )
