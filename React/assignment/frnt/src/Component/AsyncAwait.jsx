9adce68e2e8a33607a715d81ce96d78f3a5e7ea1
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-06-08 23:23:39 +0530

    comparing

--- React/assignment/frnt/src/Component/AsyncAwait.jsx
+++ React/assignment/frnt/src/Component/AsyncAwait.jsx
@@ -9,6 +9,47 @@ const AsyncAwait = () => {
 
 export default AsyncAwait
 
+// import React, { useEffect, useState } from 'react';
+
+// async function fetchData() {
+//   try {
+//     const response = await fetch('https://api.example.com/data');
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// }
+
+// function DataComponent() {
+//   const [data, setData] = useState(null);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     fetchData()
+//       .then(data => {
+//         setData(data);
+//       })
+//       .catch(error => {
+//         setError(error);
+//       });
+//   }, []);
+
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   } else if (!data) {
+//     return <div>Loading...</div>;
+//   } else {
+//     return (
+//       <div>
+//         {/* Display the fetched data */}
+//       </div>
+//     );
+//   }
+// }
+
+// export default DataComponent;
+
 
 // const [todo,setTodo]=useState([])
 // //get method...........
