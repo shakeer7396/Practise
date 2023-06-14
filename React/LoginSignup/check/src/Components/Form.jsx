7e93821e008c37336fe295772e6e88e204e82168
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-06-14 22:43:36 +0530

    Update Form.jsx

--- React/LoginSignup/check/src/Components/Form.jsx
+++ React/LoginSignup/check/src/Components/Form.jsx
@@ -1,41 +1,51 @@
-import React,{useState} from 'react'
-import FormUi from './FormUi';
+import React, { useState } from "react";
+import FormUi from "./FormUi";
 const Form = () => {
-    const [formData,setFormdata]=useState({})
-    const handleChange=(e)=>{
-        const inputName=e.target.name;
-        setFormdata({
-            ...formData,
-            [inputName]:e.target.value
-        })      
-    }
-    const handleSubmit =async(e)=>{
-        e.preventDefault()
-        let res=await fetch("http://localhost:2040/posts",{
-            method:"POST",
-            headers:{"content-type":"application/json"},
-            body:JSON.stringify(formData),
-        })
-        let data =await res.json()
-        alert("data added successfully")
-        console.log(data)
-    }
+  const [formData, setFormdata] = useState({});
+  const handleChange = (e) => {
+    const inputName = e.target.name;
+    setFormdata({
+      ...formData,
+      [inputName]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let res = await fetch("http://localhost:2040/posts", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+    let data = await res.json();
+    alert("data added successfully");
+    console.log(data);
+  };
   return (
-    <div style={{textAlign:"center"}}>
-        <form onSubmit={handleSubmit}>
-            <div>
-    <label>Name</label>
-    <input type="text" name='user' placeholder='Enter Name' onChange={handleChange} />
-    <label>Age</label>
-    <input type="number" name="age" placeholder='Enter Age' onChange={handleChange}  />
+    <div style={{ textAlign: "center" }}>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            name="user"
+            placeholder="Enter Name"
+            onChange={handleChange}
+          />
+          <label>Age</label>
+          <input
+            type="number"
+            name="age"
+            placeholder="Enter Age"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input type="submit" name="submit" />
+        </div>
+      </form>
+      <FormUi />
     </div>
-    <div>
-        <input type="submit" name="submit" />
-    </div>
-        </form>
-        <FormUi />
-    </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
