8f21dbda3bae497f05bb06f26f2222b857e490ae
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-06-14 22:44:07 +0530

    Update FormUi.jsx

--- React/LoginSignup/check/src/Components/FormUi.jsx
+++ React/LoginSignup/check/src/Components/FormUi.jsx
@@ -1,32 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const FormUi = () => {
-    const [mapData,setmapData] =useState([]);
+  const [mapData, setmapData] = useState([]);
 
-    const getData=async()=>{
-        let res = await fetch("http://localhost:2040/posts")
-        let data=await res.json();
-        console.log(data)
-        setmapData(data);
-    }
-    useEffect(()=>{
-        getData()
-    },[])
+  const getData = async () => {
+    let res = await fetch("http://localhost:2040/posts");
+    let data = await res.json();
+    console.log(data);
+    setmapData(data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div style={{textAlign:"center"}}>
-        <h2>Form UI</h2>
-    {mapData.map((el,index)=>{
-        return(
-            <div key={el.id}>
-
-            <h1><span>{`${el.id} - `}</span>{el.user}</h1>
+    <div style={{ textAlign: "center" }}>
+      <h2>Form UI</h2>
+      {mapData.map((el, index) => {
+        return (
+          <div key={el.id}>
+            <h1>
+              <span>{`${el.id} - `}</span>
+              {el.user}
+            </h1>
             <h2>{el.age}</h2>
-            </div>
-        )
-    })}
-
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default FormUi
+export default FormUi;
