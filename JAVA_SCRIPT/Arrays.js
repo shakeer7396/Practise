98eaa55112f22eec2b98766d1f40f71f39228be9
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-10-19 09:53:10 +0530

    Update Arrays.js

--- JAVA_SCRIPT/Arrays.js
+++ JAVA_SCRIPT/Arrays.js
@@ -6,6 +6,12 @@ let cars = ["bmw", "volvo", "honda"];
 
 // Array Length Property
 // As the name suggests, the length property retrieves the length of the array. 
-let len = cars.length;
-console.log(len);
+// let len = cars.length;
+// console.log(len);
 // The length of the cars array is stored in the variable len. You can view the output on the console. 
+
+// Accessing the Last Array Element
+//Since cars.length retrieves the length of the array, and since arrays use zero-based indexing, the index of the last element is one minus the length.
+// let last = cars[cars.length - 1];
+// console.log(last);
+
