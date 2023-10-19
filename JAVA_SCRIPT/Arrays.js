454453c2e7d9c3efa3a25a699a2abb7d9c825525
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-10-19 09:46:21 +0530

    Create Arrays.js

---
+++ JAVA_SCRIPT/Arrays.js
@@ -0,0 +1,11 @@
+let cars = ["bmw", "volvo", "honda"];
+// console.log(cars);
+
+//Accessing array elements
+// console.log(cars[0],cars[2]);
+
+// Array Length Property
+// As the name suggests, the length property retrieves the length of the array. 
+let len = cars.length;
+console.log(len);
+// The length of the cars array is stored in the variable len. You can view the output on the console. 
