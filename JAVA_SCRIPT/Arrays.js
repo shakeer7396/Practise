199463505bd3690c600aed272019ee8ef691df00
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-10-23 18:58:50 +0530

    Update Arrays.js

--- JAVA_SCRIPT/Arrays.js
+++ JAVA_SCRIPT/Arrays.js
@@ -1,8 +1,9 @@
-let cars = ["bmw", "volvo", "honda"];
+// let cars = ["bmw", "volvo", "honda"];
 // console.log(cars);
 
 //Accessing array elements
 // console.log(cars[0],cars[2]);
+
 
 // Array Length Property
 // As the name suggests, the length property retrieves the length of the array. 
@@ -10,10 +11,12 @@ let cars = ["bmw", "volvo", "honda"];
 // console.log(len);
 // The length of the cars array is stored in the variable len. You can view the output on the console. 
 
+
 // Accessing the Last Array Element
 //Since cars.length retrieves the length of the array, and since arrays use zero-based indexing, the index of the last element is one minus the length.
 // let last = cars[cars.length - 1];
 // console.log(last);
+
 
 // Loop Over the Array Items
 // You can also loop over an array in case you need to access multiple elements from the array at once. We use the forEach method for this, which calls a function once for each element in an array. 
@@ -23,6 +26,7 @@ let cars = ["bmw", "volvo", "honda"];
 //       });
 
 // The code above displays the name of the cars and their corresponding indices on the console
+
 
 // toString() 
 // The toString() method returns a string relating to the number. The only parameter it takes is the base index. 
@@ -35,7 +39,6 @@ let cars = ["bmw", "volvo", "honda"];
 //             }
 
 // // The code above converted every item in the array to its corresponding binary string. The output is shown below: 
-
 // // elements-console
 // toString()
 
@@ -72,3 +75,7 @@ let cars = ["bmw", "volvo", "honda"];
 // The code above must return our original array with three elements. 
 
 
+// Slice()
+// The slice() method cuts the array and returns a shallow copy of a portion of an array into a new array object. It takes in two parameters: begin and end. The array is sliced from the index specified as begin till the end index (end index excluded). The original array will not be modified.
+
+// If the end parameter is unspecified, the entire array from the begin index is sliced. 
