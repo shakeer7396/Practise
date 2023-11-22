f1b6a5d44958ff60dcea60fd3f75899a2e8e5482
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-11-22 14:04:26 +0530

    prtc

--- Counter All/React Counter-Todo/count-todo/src/Todo/Todo.jsx
+++ Counter All/React Counter-Todo/count-todo/src/Todo/Todo.jsx
@@ -1,43 +1,81 @@
-import React, { useState } from 'react'
-import DeleteItem from './DeleteItem';
+// import React, { useState } from 'react'
+// import DeleteItem from './DeleteItem';
 
-const Todo = () => {
-    const [add,setAdd] =useState('');
-    const [data,setData]= useState([]);
+// const Todo = () => {
+//     const [add,setAdd] =useState('');
+//     const [data,setData]= useState([]);
 
-    const handleChange =(e)=>{
+//     const handleChange =(e)=>{
+//         setAdd(e.target.value)
+//     }
+
+//     const handleSubmit =(e)=>{
+//         e.preventDefault();
+//         let newdata=add;
+//         setData([...data,newdata])
+//         setAdd('')
+
+//     }
+//     const handleDelete = (a) =>{
+//         const myData =data.filter((current,index)=>{
+//             return index !=a
+//         })
+//         setData(myData);
+//     }
+//   return (
+//     <div style={{marginBottom:"20px"}} >
+//         <form onSubmit={handleSubmit}>
+//         <input type="text" placeholder='Add Some Thing' value={add} onChange={handleChange} />
+//         <button type="submit">Add Item</button>
+//         </form>
+
+//         {data.map((item,index)=>{
+//             return(
+                
+//             <DeleteItem key={index} add={item} id={index} onSelect={handleDelete} />
+//             )
+//         })}
+//     </div>
+//   )
+// }
+
+// export default Todo
+
+import React,{useState} from "react";
+
+const Todo = () =>{
+    const [add,setAdd] = useState("");
+    const [data,setData] = useState([])
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        let newData = add;
+        setData([...data,newData]);
+
+
+    }
+
+    const handleChange = (e)=>{
         setAdd(e.target.value)
     }
 
-    const handleSubmit =(e)=>{
-        e.preventDefault();
-        let newdata=add;
-        setData([...data,newdata])
-        setAdd('')
-
-    }
-    const handleDelete = (a) =>{
-        const myData =data.filter((current,index)=>{
-            return index !=a
-        })
-        setData(myData);
-    }
-  return (
-    <div style={{marginBottom:"20px"}} >
-        <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Add Some Thing' value={add} onChange={handleChange} />
-        <button type="submit">Add Item</button>
+    return(
+        <div> 
+        <form onSubmit={handleSubmit}>       
+            <h1>Todo App</h1>
+            <input type="text" placeholder="Add Something" value={add} onChange={handleChange} />
+            <button type="submit">Add</button>
         </form>
 
         {data.map((item,index)=>{
             return(
-                
-            <DeleteItem key={index} add={item} id={index} onSelect={handleDelete} />
+                <div>
+                    
+                </div>
             )
         })}
-    </div>
-  )
+        </div>
+    )
 }
 
-export default Todo
-
+export default Todo;
