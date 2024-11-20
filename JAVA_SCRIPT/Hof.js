9abc68c7666d44a6a360e47663de327491307b3c
shakeer7396 <shakeer043@gmail.com>
2024-11-20 12:22:07 +0530

    Update Hof.js

--- JAVA_SCRIPT/Hof.js
+++ JAVA_SCRIPT/Hof.js
@@ -1,6 +1,6 @@
 
 /*
-
+//FILTER 77 REDUCE METHODS
 //1. Printing Even numbers using forloop
 var arr=[1,2,3,4,5,6,7];
 
@@ -104,25 +104,8 @@ console.log(output); // Printing the output
 // console.log(evout);
 
 
-/*
-// 4. multiply with 2 all values using forEach method
 
-var arr=[1,2,3,4,5];
-let bag="";
-arr.forEach(function(el){
-    bag+=el*2+" "
-
-})
-console.log(bag) //output is 2 4 6 8 10 
-
- var arr1=[1,2,3,4,5];
-arr1.forEach(function(el,acc){
-    console.log(el*2)   //output printing line by line 
-})
-*/
-
-
-// 5.  product of all the numbers
+// 4.  product of all the numbers
 
 // var arr=[1,2,3,4]
 // var prod=function(el,acc,index){
@@ -147,7 +130,7 @@ arr1.forEach(function(el,acc){
 
 
 
-//6. Multiply and cube
+//5. Multiply and cube
 
 // var arr=[1,2,3,4,5];
 // var mult=function(el,index,arra){
@@ -160,7 +143,8 @@ arr1.forEach(function(el,acc){
 // console.log(out)  //output is 27
 
 
-// 7. Add unlimited  values in arr
+// 6. Add unlimited  values in arr
+
 // var arr=[1,2,3,4,5,6,7,8,9,10,20];
 // var add=function(acc,el){
 //     return acc+el;
@@ -169,42 +153,72 @@ arr1.forEach(function(el,acc){
 // console.log(out)
 
 
-// // Given an array of strings print the length of each string
+/*
+//FOREACH  && MAP METHODS
+// 7. multiply with 2 all values using forEach method
+
+var arr=[1,2,3,4,5];
+let bag="";
+arr.forEach(function(el){
+    bag+=el*2+" "
+
+})
+console.log(bag) //output is 2 4 6 8 10 
+
+ var arr1=[1,2,3,4,5];
+arr1.forEach(function(el,acc){
+    console.log(el*2)   //output printing line by line 
+})
+
+// Print all numbers
+var arr2=[1,2,3,4,5,6,7,8,9,10,20];
+arr2.forEach(function(el){
+    console.log(el)     //output printing line by line 
+})
+*/
+
+// 8.  Given an array of strings print the length of each string
+
 // var arr=["apple", "windows", "ubuntu"];
-   
+
 //     let check=arr.map(function(el){
-//         return el;
+//         return el.length;
 //     })
-//     console.log(check)
+//     console.log(check)  //output is [ 5, 7, 6 ]
+
     
 
-//  Given an array of string generate an array whose first or last character is a
+// 9. Given an array of string generate an array whose first or last character is a
 //  Sample Input - ["assignment", "problem", "media", "upload"]
 //  Sample Output - ["assignment", "media"]
 
- //var arr=["apple", "windows", "ubuntu", "cola", "system","ola"];
+//  var arr=["apple", "windows", "ubuntu", "cola", "system","ola"];
+//  var bag="";
 //  var check=arr.map(function(el,acc,index){
 //     if(el[0]=="a"||el[el.length-1]=="a"){
-//         console.log(el)
+//         return bag+=el +" ";  //console.log(el) output is printing line by line
 //     }
 //  })
+//  console.log(bag);   //output is apple cola ola
     
-// Given an array of strings print the sum of lengths if the characters in the string are odd
+
+// 10. Given an array of strings print the sum of lengths if the characters in the string are odd
 // Sample Input - ["A", "Good", "Problem"]
 // Sample Output - 8
-// var arr=["Apple", "Windows", "Linux", "Kindle", "Quiz"];
-// var len=function(el,acc,index){
-//     return el.length;
-// }
-// var odd=function(el,acc){
-//     return el%2==1;
-// }
-// var sum=function(acc,el){
-//     return acc+el;
-// }
-// var out=arr.map(len).filter(odd).reduce(sum);
-// console.log(out)
-
+/*
+var arr=["Apple", "Windows", "Linux", "Kindle", "Quiz"];
+var len=function(el,acc,index){
+    return el.length;       //checking arr[0,1,2,.....] length 
+}
+var odd=function(el,acc){
+    return el%2==1;         //checking Odd String
+}
+var sum=function(acc,el){
+    return acc+el;          //Adding the Odd String length value
+}
+var out=arr.map(len).filter(odd).reduce(sum);
+console.log(out)        //output is 17
+*/
 
 //PRINT 1 - 100 NUMBERS;
  
@@ -212,7 +226,9 @@ arr1.forEach(function(el,acc){
 // console.log(i)
 // }
 
-// Array(100).fill( ).map( (_,i) => console.log( i + 1 ) );
+ Array(100).fill( ).map( (_,i) =>{
+console.log( i + 1 ) 
+ }) ;
 
 
 
