e2cb3349eadb67f49e2f0b7f56b6a77977377ded
shakeer7396 <shakeer043@gmail.com>
2025-04-24 12:02:13 +0530

    timer

--- JAVA_SCRIPT/SetTime_out.js
+++ JAVA_SCRIPT/SetTime_out.js
@@ -17,24 +17,16 @@
 
 // set();
 
-function set(){
-    for(var i=0;i<5;i++){
-        setTimeout(()=>{
-            console.log(i)
-        },i*3000)
+
+function x(){
+    for(var i=1;i<=5;i++){
+        setTimeout(function(){
+            console.log(i);
+            console.log("dekh")
+        },i*2000)
     }
-}
-set()
-
-// function x(){
-//     for(var i=1;i<=5;i++){
-//         setTimeout(function(){
-//             console.log(i);
-//             console.log("dekh")
-//         },i*2000)
-//     }
-// console.log("this is trail")
+console.log("this is trail")
     
-// }
+}
 
-// x()
+x()
