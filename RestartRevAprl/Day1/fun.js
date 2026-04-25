7125fbcc72d5d6e8706e9310121b33785a636212
shakeer7396 <shakeer043@gmail.com>
2026-04-25 12:53:40 +0530

    Create fun.js

---
+++ RestartRevAprl/Day1/fun.js
@@ -0,0 +1,17 @@
+// This is a simple JavaScript function that adds two numbers and logs the result to the console.
+function add(a,b){
+    return a+b;
+}
+console.log("Addition: " + add(5,5));
+
+// This is a simple JavaScript function that multiplies two numbers and logs the result to the console.
+function multiplay(a,b){
+    return a*b;
+}
+console.log("Multiplication: " + multiplay(5,5));
+
+function greet(name){
+    return "Hello, " + name + "!"; 
+}
+
+console.log("Greeting: " + greet("Shakil"));
