2d4c17951efbab6c29eff359e85a603f819d8133
shakeer7396 <shakeer043@gmail.com>
2026-04-25 13:00:31 +0530

    Update closure.js

--- RestartRevAprl/Day1/closure.js
+++ RestartRevAprl/Day1/closure.js
@@ -1,3 +1,5 @@
+//Closure is a function that has access to the parent scope, even after the parent function has closed. It allows the inner function to access and manipulate variables from the outer function, creating a persistent state.
+
 function Outer(){
     var count = 0;
    return function Inner(){
@@ -8,3 +10,4 @@ function Outer(){
 const fn=Outer();
 fn();
 fn();
+
